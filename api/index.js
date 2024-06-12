cc6d5ea8f99cc9b4e68e282bd430c202871aa548
novice <seungyeon5374@gmail.com>
2024-06-12 10:31:03 +0900

    몽고 아틀라스 연동

--- api/index.js
+++ api/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+console.log('몽고아틀라스주소: ',process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>console.log('데이터베이스 연결 성공'))
+.catch(e => console.log(`데이터베이스 연결 실패 ${e}`))
 
 app.get('/',(req,res)=>res.send('express on vercel'))
 
